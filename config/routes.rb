328c36297c60c0b9631a4b76d1fb3536356b8c78
Giancarlo <gsoliscastillo@gmail.com>
2016-03-14 11:34:50 -0500

    apartment test app

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+class SubdomainConstraint
+  def self.matches?(request)
+    subdomains =%w{ www admin }
+    request.subdomain.present? && !subdomains.include?(request.subdomain)
+  end
+end
+
+Rails.application.routes.draw do
+  constraints SubdomainConstraint do
+    resources :projects
+  end
+  resources :users
+end
